7f5019e616292ca6c1473a022529fd1aa621634e
fo0 <fo0@fo0.me>
2020-05-02 10:33:26 -0700

    init

--- SpringCloudConsulExample/src/main/java/com/fo0/spring/cloud/consul/example/SpringCloudConsulExampleApplication.java
+++ SpringCloudConsulExample/src/main/java/com/fo0/spring/cloud/consul/example/SpringCloudConsulExampleApplication.java
@@ -2,29 +2,34 @@ package com.fo0.spring.cloud.consul.example;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.log4j.Log4j2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@RestController
+//@RestController
+@Log4j2
+@ConfigurationPropertiesScan("com.fo0.spring.cloud.consul.example.config")
 public class SpringCloudConsulExampleApplication {
+
+	public static final String NAME = "example-service";
 
 	public static void main(String[] args) {
 		// @formatter:off
 		new SpringApplicationBuilder(SpringCloudConsulExampleApplication.class)
-				.properties("spring.application.name=example-service")
+				.properties("spring.application.name="+NAME)
 				.properties("spring.cloud.consul.host=127.0.0.1")
 				.properties("spring.cloud.consul.port=8500")
-				.properties("spring.cloud.consul.discovery.instanceId=example-service:${random.value}")
+				.properties("spring.cloud.consul.discovery.instanceId=" + NAME + ":${random.value}")
 			.run(args);
 		// @formatter:on
 	}
 
-	@RequestMapping("/")
-	public String home() {
-		return "Hello world";
-	}
+//	@RequestMapping("/")
+//	public String home() {
+//		return "Hello world";
+//	}
 
 }
