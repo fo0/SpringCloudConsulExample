b82c7ba7969c26e2d134126e3ba659eb18921e0a
fo0 <fo0@fo0.me>
2020-05-02 16:29:36 -0700

    added healthcheck

--- SpringCloudConsulExample/src/main/java/com/fo0/spring/cloud/consul/example/Main.java
+++ SpringCloudConsulExample/src/main/java/com/fo0/spring/cloud/consul/example/Main.java
@@ -10,17 +10,19 @@ import lombok.extern.log4j.Log4j2;
 @EnableDiscoveryClient
 //@RestController
 @Log4j2
-public class SpringCloudConsulExampleApplication {
+public class Main {
 
 	public static final String NAME = "example-service";
 
 	public static void main(String[] args) {
 		// @formatter:off
-		new SpringApplicationBuilder(SpringCloudConsulExampleApplication.class)
+		new SpringApplicationBuilder(Main.class)
 				.properties("spring.application.name=" + NAME)
 				.properties("spring.cloud.consul.host=127.0.0.1")
 				.properties("spring.cloud.consul.port=8500")
 				.properties("spring.cloud.consul.discovery.instanceId=" + NAME + ":${random.value}")
+				.properties("spring.cloud.consul.discovery.healthCheckPath=/my-health-check")
+				.properties("spring.cloud.consul.discovery.healthCheckPathInterval=20s")
 			.run(args);
 		// @formatter:on
 	}
